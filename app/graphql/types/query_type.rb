945dd713760586d0a7d494c0f71f93f013bb16e4
csolallo <csolallo@penguin>
2020-04-24 13:13:22 -0700

    initial stub of the resume object

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,12 +1,21 @@
 module Types
   class QueryType < GraphQL::Schema::Object
-    field :person, PersonType, null: false do
-      description "Find a person by ID"
+    field :person, PersonType, null: true do
+      description 'Query a person by name'
+      argument :name, String, required: true
+    end
+
+    field :resume, ResumeType, null: true do
+      description 'Query for a person''s resume'
       argument :name, String, required: true
     end
 
     def person(name:)
       Person.find_by_name(name)
+    end
+
+    def resume(name:)
+      Person.find_by_name(name).resume
     end
   end
 end
---
+++ app/graphql/types/resume_type.rb
@@ -0,0 +1,5 @@
+class Types::ResumeType < Types::BaseObject
+  description "Top-level object for a resume"
+
+  field :id, ID, null: false
+end
