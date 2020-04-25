9b90e3bfb173596ba3f30890a248c8320a2a59ec
csolallo <csolallo@penguin>
2020-04-25 08:32:31 -0700

    adding at_company scope

--- app/models/resume.rb
+++ app/models/resume.rb
@@ -1,6 +1,6 @@
 class Resume < ApplicationRecord
   belongs_to :person
-  has_many :jobs, -> { order 'start_date DESC' }, :class_name => 'Role'
+  has_many :jobs, -> { order start_date: :desc }, :class_name => 'Role'
 
   scope :for_person, -> (person) { where(:person => person) }
 end
--- app/models/role.rb
+++ app/models/role.rb
@@ -5,5 +5,6 @@ class Role < ApplicationRecord
   has_many :accomplishments, as: :associated
 
   scope :in_resume, -> (resume){ where(resume: resume) }
+  scope :at_company, -> (company){ where(company: company).order(start_date: :desc) }
   scope :since, -> (start){ where('`roles`.`start_date` >= ?', start).order(start_date: :desc) }
 end
