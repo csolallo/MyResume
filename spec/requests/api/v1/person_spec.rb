c24c6a5b711bd06796dd1ba0f629258324adbc21
Carlos Sola-Llonch <csolallo@gmail.com>
2025-08-22 18:15:58 -0700

    added remaining person controller endpoints

--- spec/requests/api/v1/person_spec.rb
+++ spec/requests/api/v1/person_spec.rb
@@ -113,4 +113,57 @@ RSpec.describe 'person endpoints', type: :request do
       response 404, 'queried using an invalid person id', &not_found_proc
     end
   end
+
+  path '/api/v1/person/{uuid}/highlights' do
+    parameter name: :uuid, in: :path, type: :string, required: true, description: 'id of the person being queried'
+
+    get 'career highlights' do
+      description 'Bulleted list of career highlights for person'
+      tags 'Person'
+    
+      response 200, 'queried with a valid person id' do
+        produces 'application/json'
+        let (:uuid) { Me }
+
+        schema type: 'array', items: 
+        {
+          type: 'string',
+          example: 'Over a decade of engineering leadership'
+        }
+
+        run_test!
+      end
+
+      response 404, 'queried using invalid person id', &not_found_proc
+    end
+
+  end
+
+  path '/api/v1/person/{uuid}/resume' do
+    parameter name: :uuid, in: :path, type: :string, required: true, description: 'id of the person being queried'
+
+    get 'resume id' do
+      description 'get the id of the resume associated with person'
+      tags 'Person'
+
+      response 200, 'queried with valid person id' do
+        produces 'application/json'
+        let (:uuid) { Me }
+
+        schema type: 'object',
+          required: %w(resume),
+          properties: {
+            resume: {
+              type: 'integer',
+              example: 1
+            }
+          }
+
+        run_test!
+      end
+
+      response 404, 'queried using an invalid person id', &not_found_proc
+    end
+  end
+
 end
